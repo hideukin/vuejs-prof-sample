d8e225f54bece17931893add6e34ece91b27eabb
hideukin <hideukin@gmail.com>
2021-05-01 00:32:23 +0900

    Add todo samcple.

---
+++ src/todo/app.js
@@ -0,0 +1,34 @@
+Vue.createApp({
+  data: function () {
+    return {
+      todoTitle: "",
+      todoDescription: "",
+      order: "desc",
+      categoryName: "",
+    };
+  },
+  computed: {
+    canCreateTodo: function () {
+      return this.todoTitle !== "";
+    },
+    canCreateCategory: function () {
+      return this.categoryName !== "";
+    },
+  },
+  methods: {
+    createTodo: function () {
+      if (!this.canCreateTodo) {
+        return;
+      }
+      this.todoTitle = "";
+      this.todoDescription = "";
+      this.todoCategories = [];
+    },
+    createCategory: function () {
+      if (!this.canCreateCategory) {
+        return;
+      }
+      this.categoryName = "";
+    },
+  },
+}).mount("#app");
