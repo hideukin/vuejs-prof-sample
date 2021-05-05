75e036e4fa245b6c6050ec6ce854e468bc4dd62f
hideukin <hideukin@gmail.com>
2021-05-05 18:53:03 +0900

    メソッドに切り出し

---
+++ src/sample2.js
@@ -0,0 +1,24 @@
+const countUpButton = {
+  template: "#btn-template",
+  methods: {
+    onClick: function () {
+      this.$emit("count-up");
+    },
+  },
+};
+
+Vue.createApp({
+  data: function () {
+    return {
+      count: 0,
+    };
+  },
+  components: {
+    "count-up-button": countUpButton,
+  },
+  methods: {
+    countUp: function () {
+      this.count += 1;
+    },
+  },
+}).mount("#app");
